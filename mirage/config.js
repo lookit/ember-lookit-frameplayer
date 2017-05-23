7b7ee4a454516ff59b98f30eb07ec0a59c79d5b0
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-05-23 18:52:06 -0400

    Mock user detail response from API.

--- mirage/config.js
+++ mirage/config.js
@@ -25,6 +25,73 @@ export default function() {
 
   */
   this.namespace = '/api';
+  this.get('/users/:id', function() {
+     return {
+          "data": {
+            "type": "user",
+            "id": "abcde",
+            "relationships": {
+                "profiles": {
+                    "data": [
+                        {
+                          "profileId": "fghij",
+                          "additionalInformation": "",
+                          "ageAtBirth": "40 or more weeks",
+                          "gender": "female",
+                          "deleted": false,
+                          "birthday": "2015-05-01T04:00:00.000Z",
+                          "firstName": "Test Child"
+                        }
+
+                    ]
+                }
+            },
+            "attributes": {
+              "additional-comments": null,
+              "demographicsGender": "female",
+              "spouse-education-level": "graduate or professional degree",
+              "profiles": [
+                {
+                  "profileId": "fghij",
+                  "additionalInformation": "",
+                  "ageAtBirth": "40 or more weeks",
+                  "gender": "female",
+                  "deleted": false,
+                  "birthday": "2015-05-01T04:00:00.000Z",
+                  "firstName": "Test Child"
+                }
+              ],
+              "state": "TX",
+              "annual-income": "over 200000",
+              "number-of-guardians-explanation": null,
+              "name": null,
+              "number-of-books": 1000000000,
+              "education-level": "graduate or professional degree",
+              "age": "70 or over",
+              "number-of-guardians": "2",
+              "password": "$2b$12$SbNsmUQzpd4.PCp2x7ywq.l11TQU3WADvpD0kEGbJZwNPEsy4xub.",
+              "languages-spoken-at-home": null,
+              "email": "pattison.dawn.r@gmail.com",
+              "child-birthdays": [
+
+              ],
+              "race-identification": [
+                "White"
+              ],
+              "number-of-children": null,
+              "density": null,
+              "country": "US"
+            },
+            "meta": {
+              "permissions": "CRUD",
+              "created-on": "2017-05-17T14:44:11.254007",
+              "created-by": "jam-experimenter:accounts-pattisdr",
+              "modified-on": "2017-05-17T15:00:19.406697",
+              "modified-by": "jam-experimenter:accounts-pattisdr"
+            }
+          }
+      };
+  });
   this.get('/studies/:id', function() {
       return {
           'data': {
@@ -63,6 +130,6 @@ export default function() {
                   "exitUrl": "https:\/\/staging.osf.io\/"
               }
           }
-      }
+      };
   });
 }
