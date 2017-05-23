950708b96dd0a61d8542046ff74c5f8751cb9ff3
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-05-23 09:51:45 -0400

    Start building study model.

--- app/models/study.js
+++ app/models/study.js
@@ -1,5 +1,18 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    name: DS.attr(),
+    shortDescription: DS.attr(),
+    longDescription: DS.attr(),
+    criteria: DS.attr(),
+    duration: DS.attr(),
+    contactInfo: DS.attr(),
+    image: DS.attr(),
+    // organization: DS.attr(),
+    blocks: DS.attr(),
+    state: DS.attr(),
+    public: DS.attr(),
+    displayFullScreen: DS.attr(),
+    structure: DS.attr(),
+    exitURL: DS.attr()
 });
--- app/routes/participate.js
+++ app/routes/participate.js
@@ -2,12 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
-        return {
-            id: params.study_id,
-            type: 'study',
-            title: 'Hello',
-            description: 'this study'
-
-        }
+        return this.get('store').findRecord('study', params.study_id);
     }
 });
