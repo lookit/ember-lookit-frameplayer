8ef54a4a3501cab00374493ad13c3a06ef97fede
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-05-26 10:47:52 -0400

    Copy isDirty function from Lookit participate controller.

--- app/controllers/participate.js
+++ app/controllers/participate.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    isDirty: function() {
+        // TODO: check the response model to see if it contains unsaved data
+        var response = this.get('response');
+        return response.get('hasDirtyAttributes');
+    },
     actions: {
         saveResponse(payload, callback) {
             var response = this.get('response');
