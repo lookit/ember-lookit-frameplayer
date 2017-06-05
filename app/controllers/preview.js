81a7a2b857697fe89daef4a6c3e581a751e543ea
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-06-05 15:51:22 -0400

    Adapt preview controller, route handler, and template from experimenter.

---
+++ app/controllers/preview.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+// Adapted from Experimenter's preview controller https://github.com/CenterForOpenScience/experimenter/blob/develop/app/controllers/experiments/info/preview.js
+export default Ember.Controller.extend({
+    breadCrumb: 'Preview',
+    experiment: null,
+    session: null,
+    isDirty: function() {
+        return this.get('model.hasDirtyAttributes');
+    },
+
+    _resolve: null,
+    previewData: null,
+    showData: false,
+    showPreviewData(session) {
+        return new Ember.RSVP.Promise((resolve) => {
+            this.set('previewData', JSON.stringify(session.toJSON(), null, 4));
+            this.set('showData', true);
+            this.set('_resolve', resolve);
+        });
+    },
+    actions: {
+        toggleData() {
+            this.toggleProperty('showData');
+            this.get('_resolve')();
+        }
+    }
+});
---
+++ app/routes/preview.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+import WarnOnExitRouteMixin from 'exp-player/mixins/warn-on-exit-route';
+import FramePlayerRoute from '../mixins/frame-player-route';
+
+// Adapted from Experimenter preview route https://github.com/CenterForOpenScience/experimenter/blob/develop/app/routes/experiments/info/preview.js
+export default Ember.Route.extend(WarnOnExitRouteMixin, FramePlayerRoute, {
+    _createStudyResponse() {
+        const response = this._super();
+        response.setProperties({
+            id: 'PREVIEW_DATA_DISREGARD'
+        });
+        return response.reopen({
+            save() {
+                    console.log('Preview Data Save:', this.toJSON());
+                    if (this.get('completed')) {
+                        var controller = Ember.getOwner(this).lookup('controller:preview');
+                        controller.showPreviewData(this).then(() => {
+                            // Override the WarnOnExitMixin's behavior
+                            controller.set('forceExit', true);
+                            // TODO transition to study detail or study create?
+                            // return _this.transitionTo('experiments.info');
+                        });
+                        return Ember.RSVP.reject();
+                    } else {
+                        return Ember.RSVP.resolve(this);
+                    }
+                }});
+        }
+});
