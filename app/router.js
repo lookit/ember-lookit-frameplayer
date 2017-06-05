43d66cbe938d7c0eb7dd2ad3637c21cfe0d53fc0
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-06-05 15:49:51 -0400

    Add preview route.

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('participate', {path: '/studies/:study_id/:profile_id'});
+  this.route('preview', {path: '/studies/:study_id/:profile_id/preview'});
   this.route('page-not-found', { path: '/*wildcard' });
   this.route('page-not-found');
 });
