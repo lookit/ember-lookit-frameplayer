cdef40373988b276b8c18ea37909d37ddf429054
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-07-12 15:40:41 -0500

    Add trailing slash to API requests.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
+import config from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
-    namespace: 'api/v1'
+    host: config.host,
+    namespace: 'api/v1',
+    buildURL: function(type, id, record) {
+        // Add trailing slash to requests
+       return this._super(type, id, record) + '/';
+     }
 });
