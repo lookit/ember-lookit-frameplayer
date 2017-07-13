d6a57fa5236051d677a837f7e07a3d164d143704
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-07-13 08:16:10 -0500

    Add localhost:8000 as host on development.

--- config/environment.js
+++ config/environment.js
@@ -25,6 +25,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
+      ENV.host = 'http://localhost:8000';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
