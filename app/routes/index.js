317ab9f3d49fc65ade7e55fd08f997c10255096a
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-07-14 17:15:48 -0500

    Remove demo route.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
-// TEMPORARY ROUTING - FOR DEMO PURPOSES ONLY
+
 export default Ember.Route.extend({
-    beforeModel() {
-        window.location.pathname = '/studies/12345/0f8221bc0de74ed2b9e6803860d9b962'
-    }
 });
