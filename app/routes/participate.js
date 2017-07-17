dc4edf93e1cccf03f215f1ec1eeed8ba6fa6f211
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-07-17 11:05:20 -0500

    Add authenticatedroutemixin to applicable routes.

--- app/routes/participate.js
+++ app/routes/participate.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import WarnOnExitRouteMixin from 'exp-player/mixins/warn-on-exit-route';
 import FramePlayerRoute from '../mixins/frame-player-route';
 
 // Adapted from Lookit participate route https://github.com/CenterForOpenScience/lookit/blob/develop/app/routes/participate.js
-export default Ember.Route.extend(WarnOnExitRouteMixin, FramePlayerRoute, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, WarnOnExitRouteMixin, FramePlayerRoute, {
     beforeModel (transition) {
         if (!this.get('session.data.child')) {
             window.console.log('No child in injected session, so transitioning to study detail');
--- app/routes/preview.js
+++ app/routes/preview.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import WarnOnExitRouteMixin from 'exp-player/mixins/warn-on-exit-route';
 import FramePlayerRoute from '../mixins/frame-player-route';
 
 // Adapted from Experimenter preview route https://github.com/CenterForOpenScience/experimenter/blob/develop/app/routes/experiments/info/preview.js
-export default Ember.Route.extend(WarnOnExitRouteMixin, FramePlayerRoute, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, WarnOnExitRouteMixin, FramePlayerRoute, {
     _createStudyResponse() {
         const response = this._super();
         response.setProperties({
