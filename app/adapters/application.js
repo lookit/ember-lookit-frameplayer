dbb2cf5fa688f3b49e26669a9b617d438ee1cb19
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-07-28 15:07:38 -0500

    Add CSRFTOKEN to header.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,12 +1,19 @@
 import DS from 'ember-data';
 import config from '../config/environment';
 import HasManyQuery from 'ember-data-has-many-query';
+import Ember from 'ember';
 
 export default DS.JSONAPIAdapter.extend(HasManyQuery.RESTAdapterMixin, {
     host: config.host,
     namespace: 'api/v1',
     buildURL: function(type, id, record) {
         // Add trailing slash to Lookit API requests
-       return this._super(type, id, record) + '/';
-     }
+        return this._super(type, id, record) + '/';
+    },
+    headers: Ember.computed(function() {
+        // Add cookie to http header
+        return {
+            'X-CSRFTOKEN': Ember.get(document.cookie.match(/csrftoken\=([^;]*)/), '1')
+        };
+    }).volatile()
 });
