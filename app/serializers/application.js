09acf68f8952c639878d77d8e34a186e86c6e32d
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-09-06 10:25:50 -0500

    Fix spacing.

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -5,19 +5,18 @@ export default DS.JSONAPISerializer.extend({
     keyForAttribute: function(attr) {
         return Ember.String.underscore(attr);
     },
-   keyForRelationship(key) {
-       return Ember.String.underscore(key);
-   },
-   normalizeArrayResponse(store, primaryModelClass, payload) {
-       // Adapted from CenterForOpenScience/ember-osf/serializers/osf-serializer to handle pagination from Lookit API -
-       //
-       // Ember data does not yet support pagination. For any request that returns more than one result, add pagination data
-       // under meta, and then calculate total pages to be loaded.
-       let documentHash = this._super(...arguments);
-       documentHash.meta = documentHash.meta || {};
-       documentHash.meta.pagination = Ember.$.extend(true, {}, Ember.get(payload.links || {}, 'meta'));
-       documentHash.meta.total = Math.ceil(documentHash.meta.pagination.count / 10); // Assuming 10 per page
-       return documentHash;
-   }
-
+    keyForRelationship(key) {
+        return Ember.String.underscore(key);
+    },
+    normalizeArrayResponse(store, primaryModelClass, payload) {
+        // Adapted from CenterForOpenScience/ember-osf/serializers/osf-serializer to handle pagination from Lookit API -
+        //
+        // Ember data does not yet support pagination. For any request that returns more than one result, add pagination data
+        // under meta, and then calculate total pages to be loaded.
+        let documentHash = this._super(...arguments);
+        documentHash.meta = documentHash.meta || {};
+        documentHash.meta.pagination = Ember.$.extend(true, {}, Ember.get(payload.links || {}, 'meta'));
+        documentHash.meta.total = Math.ceil(documentHash.meta.pagination.count / 10); // Assuming 10 per page
+        return documentHash;
+    }
 });
