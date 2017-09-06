eb951e306f44310aab2726dd3b83c96cae97f5dd
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-09-06 10:29:28 -0500

    Fix spacing.

--- app/serializers/response.js
+++ app/serializers/response.js
@@ -1,9 +1,9 @@
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
-  serialize(snapshot, options) {
-    let json = this._super(snapshot, options);
-    delete json.data.relationships['demographic_snapshot']
-    return json;
-  },
+    serialize(snapshot, options) {
+        let json = this._super(snapshot, options);
+        delete json.data.relationships['demographic_snapshot']
+        return json;
+    },
 });
