afb68464fdf404c45b211920941328a38ab101b3
Kim Scott <kimscott@mit.edu>
2019-02-21 12:08:31 -0500

    get tests to run (not all passing yet)

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,13 +1,7 @@
-import resolver from './helpers/resolver';
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-import {
-  setResolver
-} from 'ember-qunit';
-
-setResolver(resolver);
 
 setApplication(Application.create(config.APP));
 
