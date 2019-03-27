a8fd9e8ed76ecbfe9ea50908c76549930b8df861
Rico Rodriguez <rico.l.rodriguez@gmail.com>
2019-03-27 14:58:33 -0400

    build now controlled entirely through env file

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,7 +4,7 @@ import HasManyQuery from 'ember-data-has-many-query';
 import Ember from 'ember';
 
 export default DS.JSONAPIAdapter.extend(HasManyQuery.RESTAdapterMixin, {
-    host: config.host,
+    host: config.APP.host,
     namespace: 'api/v1',
     buildURL: function(type, id, record) {
         // Add trailing slash to Lookit API requests
@@ -12,8 +12,15 @@ export default DS.JSONAPIAdapter.extend(HasManyQuery.RESTAdapterMixin, {
     },
     headers: Ember.computed(function() {
         // Add cookie to http header
-        return {
+
+        let headers = {
             'X-CSRFTOKEN': Ember.get(document.cookie.match(/csrftoken\=([^;]*)/), '1') // eslint-disable-line no-useless-escape
         };
+
+        if (config.APP.apiKey) {
+            headers['Authorization'] = config.APP.apiKey;
+        }
+
+        return headers;
     }).volatile()
 });
