7a32943d6129844d1db2dc46a4aa266a3bc247b1
glebiakovlev <g-iakovlev@northwestern.edu>
2019-04-29 22:36:08 -0500

    Reduce game rounds to 10 now

--- app/components/exp-lookit-games/utils.js
+++ app/components/exp-lookit-games/utils.js
@@ -16,7 +16,7 @@ export default class Utils{
     static get  paddleSpeed() {return 4;}
     static get  ballMass() {return 0.1;}
     static get  restitution() {return -1.2;}
-    static get  gameRounds() {return 30;}
+    static get  gameRounds() {return 10;}
     static get  gravityFactor() {return 1;}
 
     //Sound Resources
