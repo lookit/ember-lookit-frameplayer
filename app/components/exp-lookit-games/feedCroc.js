99619ed5659ff1a8ebc807b3964bfb6e3e710e08
glebiakovlev <g-iakovlev@northwestern.edu>
2019-05-14 09:25:52 -0500

    Randomize trajectory angle,reduce bounce rate.

--- app/components/exp-lookit-games/feedCroc.js
+++ app/components/exp-lookit-games/feedCroc.js
@@ -33,222 +33,230 @@ let initSoundPlaying = true;
 export default class FeedCroc extends Base {
 
 
-    /**
-     * Constructor to get parameters from caller
-     * @method constructor
-     * @constructor constructor
-     * @param context from component
-     * @param document object from component
-     */
-    constructor(context, document) {
+  /**
+   * Constructor to get parameters from caller
+   * @method constructor
+   * @constructor constructor
+   * @param context from component
+   * @param document object from component
+   */
+  constructor(context, document) {
 
-        super(context, document);
+    super(context, document);
 
-    }
+  }
 
-    /**
-     * Main point to start the game.
-     * Initialize static parameters and preload sounds here
-     * @method init
-     */
-    init() {
-        super.init();
+  /**
+   * Main point to start the game.
+   * Initialize static parameters and preload sounds here
+   * @method init
+   */
+  init() {
+    super.init();
 
-        paddle = {
+    paddle = {
 
-            position: {x: super.paddleWidth * 10 - 5, y: this.canvas.height / 2.5 + this.canvas.height / 2 - super.paddleHeight},
-            paddleLastMovedMillis: 100,
-            velocity: super.Utils.paddleSpeed
+      position: {x: super.paddleWidth * 10-5, y: this.canvas.height / 2.5 + this.canvas.height / 2 - super.paddleHeight},
+      paddleLastMovedMillis: 100,
+      velocity: super.Utils.paddleSpeed
 
-        };
+    };
 
-        target = {
+    target = {
 
-            dimensions: {width: this.canvas.width / 5, height: this.canvas.width / 5},
-            position: {x: this.canvas.width - this.canvas.width / 3.5, y: 10},
-            imageURL: super.Utils.crocStartImage,
-            imageTargetReachedURL: super.Utils.crocdoneImage
+      dimensions: {width: this.canvas.width / 5, height: this.canvas.width / 5},
+      position: {x: this.canvas.width - this.canvas.width / 3.5, y: 10},
+      imageURL: super.Utils.crocStartImage,
+      imageTargetReachedURL: super.Utils.crocdoneImage
 
-        };
+    };
 
-        bounceSound = new Audio(super.Utils.bouncingSound);
-        bounceSound.load();
+    bounceSound = new Audio(super.Utils.bouncingSound);
+    bounceSound.load();
 
-        goodJob = new Audio(super.Utils.crocSlurpSound);
-        goodJob.load();
+    goodJob = new Audio(super.Utils.crocSlurpSound);
+    goodJob.load();
 
-        ballCatchFail = new Audio(super.Utils.ballcatchFailSound);
-        ballCatchFail.load();
+    ballCatchFail = new Audio(super.Utils.ballcatchFailSound);
+    ballCatchFail.load();
 
-        audio = new Audio(super.Utils.rattleSound);
-        audio.load();
-        audio.addEventListener('onloadeddata', this.initGame(), false);
+    audio = new Audio(super.Utils.rattleSound);
+    audio.load();
+    audio.addEventListener('onloadeddata', this.initGame(), false);
 
-    }
-
-
+  }
 
 
-    /**
-     *
-     * Main loop of the game.
-     * Set initial position of the ball in a box and starting rattling sound (initSoundPlaying).
-     * After that  start ball trajectory.
-     * If ball hits the target or missed the target (hits any screen edge) wait util user places the paddle to starting position and move
-     * the ball to initial position.
-     * Increase the score if ball hits the target.
-     * @method loop
-     */
-    loop() {
 
-        super.loop();
 
-        super.createBallBox();
-        super.createPaddleBox(super.paddleWidth * 10, this.canvas.height / 2.5 + this.canvas.height / 2 - super.paddleWidth * 1.3);
-        super.drawPaddle(paddle.position.x + 5, paddle.position.y);
-        this.drawImage(target, target.imageURL);
-        super.paddleMove(paddle);
-        this.paddleBallCollision();
+  /**
+   *
+   * Main loop of the game.
+   * Set initial position of the ball in a box and starting rattling sound (initSoundPlaying).
+   * After that  start ball trajectory.
+   * If ball hits the target or missed the target (hits any screen edge) wait util user places the paddle to starting position and move
+   * the ball to initial position.
+   * Increase the score if ball hits the target.
+   * @method loop
+   */
+  loop() {
 
-        let hitTheTarget = this.collisionDetection();
-        let hitTheWall = super.wallCollision(ball);
+    super.loop();
 
-        if (hitTheTarget || hitTheWall || super.gameOver) {
-            // Remove ball and show in the starting point,
-            //User should set the paddle to initial position , call stop after that
+    super.createBallBox();
+    super.createPaddleBox(super.paddleWidth * 10, this.canvas.height / 2.5 + this.canvas.height / 2 - super.paddleWidth*1.3);
+    super.drawPaddle(paddle.position.x + 5, paddle.position.y);
+    this.drawImage(target, target.imageURL);
+    super.paddleMove(paddle);
+    this.paddleBallCollision();
 
-            if (hitTheTarget) {
-                if (!super.gameOver && goodJob.readyState === 4) {
+    let hitTheTarget = this.collisionDetection();
+    let hitTheWall = super.wallCollision(ball);
 
-                    goodJob.play();
-                }
-                this.drawImage(target, target.imageTargetReachedURL);
+    if (hitTheTarget || hitTheWall || super.gameOver) {
+      // Remove ball and show in the starting point,
+      //User should set the paddle to initial position , call stop after that
 
-            } else {
-                if (!super.gameOver) {
+      if (hitTheTarget) {
+        if (!super.gameOver && goodJob.readyState === 4) {
 
-                    ballCatchFail.play();
-                }
-            }
-
-            super.moveBallToStart(ball, true);
-            super.paddleAtZero(paddle, hitTheTarget);
-
-        } else {
-
-            if (initSoundPlaying) {
-
-                super.moveBallToStart(ball, false);
-
-            } else {
-
-                super.ballTrajectory(ball);
-
-            }
+          goodJob.play();
         }
+        this.drawImage(target, target.imageTargetReachedURL);
 
-    }
+      } else {
+        if (!super.gameOver) {
 
-    /**
-     *
-     * Handle paddle collision here
-     * Adjust velocity to the ball by restitution factor
-     * @method paddleBallCollision
-     */
-    paddleBallCollision() {
-        if (ball.position.y >= (paddle.position.y - super.paddleHeight / 4) && ball.position.y < (paddle.position.y + super.paddleHeight / 4)) {
-            if ((ball.position.x > paddle.position.x - super.paddleWidth * 1.3 && ball.position.x < paddle.position.x + super.paddleWidth * 1.3)) {
-                if (new Date().getTime() - paddle.paddleLastMovedMillis > 130) {
-                    bounceSound.play();
-
-                    ball.velocity.y *= ball.restitution;
-                    ball.velocity.x *= -ball.restitution;
-                    ball.position.y = paddle.position.y - ball.radius;
-                    paddle.paddleLastMovedMillis = new Date().getTime();
-                }
-            }
+          ballCatchFail.play();
         }
+      }
+
+      super.moveBallToStart(ball, true);
+      super.paddleAtZero(paddle, hitTheTarget);
+
+    } else {
+
+      if (initSoundPlaying) {
+
+        super.moveBallToStart(ball, false);
+
+      } else {
+
+        super.ballTrajectory(ball);
+
+      }
     }
 
+  }
 
-    /**
-     *
-     * Check if ball reached the target
-     * @method collisionDetection
-     * @return {boolean}
-     */
-    collisionDetection() {
-
-        if ((ball.position.y < target.position.y + target.dimensions.height) && (ball.position.y > target.position.y + target.dimensions.height / 1.6) && ball.position.x > target.position.x + 40 && ball.position.x < target.position.x + 80) {
-            return true;
+  /**
+   *
+   * Handle paddle collision here
+   * Adjust velocity to the ball by restitution factor
+   * @method paddleBallCollision
+   */
+  paddleBallCollision() {
+    if (ball.position.y > paddle.position.y  - ball.radius && ball.position.y < paddle.position.y + ball.radius  ) {
+      if ((ball.position.x > paddle.position.x -super.paddleWidth/2 && ball.position.x < paddle.position.x + super.paddleWidth*1.3)) {
+        if (new Date().getTime() - paddle.paddleLastMovedMillis > 200) {
+          bounceSound.play();
+          ball.velocity.y *= ball.restitution;
+          ball.velocity.x *= -ball.restitution;
+          paddle.paddleLastMovedMillis = new Date().getTime();
+          //
+          ball.position.x += ball.velocity.x * super.Utils.frameRate * 100;
+          ball.position.y -= ball.velocity.y * super.Utils.frameRate * 100;
+          this.ctx.translate(ball.position.x, ball.position.y);
+          this.ctx.beginPath();
+          this.ctx.arc(0, 0, ball.radius, 0, Math.PI * 2, true);
+          this.ctx.fillStyle = ball.color;
+          this.ctx.fill();
+          this.ctx.closePath();
         }
+      }
+    }
+  }
 
-        return false;
+
+  /**
+   *
+   * Check if ball reached the target
+   * @method collisionDetection
+   * @return {boolean}
+   */
+  collisionDetection() {
+
+    if ((ball.position.y < target.position.y + target.dimensions.height) && (ball.position.y > target.position.y + target.dimensions.height / 1.6) && ball.position.x > target.position.x + 40 && ball.position.x < target.position.x + 80) {
+      return true;
     }
 
+    return false;
+  }
 
-    /**
-     *
-     * Initialize each game round with initial object parameters
-     * Randomize number of obstructions
-     * Reset the sounds sources for older browser versions
-     * @method initGame
-     */
-    initGame() {
-        let trajectories = [
 
-            {velocity: {x: 3.2, y: -6.8}},
-            {velocity: {x: 3.1, y: -7.2}},
-            {velocity: {x: 3.0, y: -7.7}},
-            {velocity: {x: 2.8, y: -7.6}}
+  /**
+   *
+   * Initialize each game round with initial object parameters
+   * Randomize number of obstructions
+   * Reset the sounds sources for older browser versions
+   * @method initGame
+   */
+  initGame() {
+    let Angle = (65*(Math.PI)/180);
+    const  trajectories = [
+      {velocity: {x: 4.0* (55*(Math.PI)/180), y: -8.8*Math.sin(Angle)}},
+      {velocity: {x: 4.3* (50*(Math.PI)/180), y: -7.7*Math.sin(Angle)}},
+      {velocity: {x: 5.6* (35*(Math.PI)/180), y: -7.5*Math.sin(Angle)}}
+    ];
 
-        ];
-        let trajectory = trajectories[Math.floor(Math.random() * trajectories.length)];
+    let index = Math.floor(Math.random() * trajectories.length);
+    let trajectory = trajectories[index];
 
-        ball = {
+    ball = {
 
-            position: {x: super.paddleWidth * 5 + 20, y: (this.canvas.height - super.paddleWidth * 2)},
-            velocity: {x: trajectory.velocity.x, y: trajectory.velocity.y},
-            mass: super.Utils.ballMass,
-            radius: 10,
-            restitution: -1.5,
-            color: '#dadd0f'
+      position: {x: super.paddleWidth * 5 + 20, y: (this.canvas.height - super.paddleWidth * 2)},
+      velocity: {x: trajectory.velocity.x, y: trajectory.velocity.y},
+      mass: super.Utils.ballMass,
+      radius: 10,
+      restitution: -1.55,
+      color: '#dadd0f'
 
-        };
+    };
 
-        initSoundPlaying = true;
-        goodJob.src = super.Utils.crocSlurpSound;
-        ballCatchFail.src = super.Utils.ballcatchFailSound;
-        bounceSound.src = super.Utils.bouncingSound;
-        audio.src = super.Utils.rattleSound;
-        audio.play();
-        audio.addEventListener('ended', function () {
+    initSoundPlaying = true;
+    goodJob.src = super.Utils.crocSlurpSound;
+    ballCatchFail.src = super.Utils.ballcatchFailSound;
+    bounceSound.src = super.Utils.bouncingSound;
+    audio.src = super.Utils.rattleSound;
+    audio.play();
+    audio.addEventListener('ended', function () {
 
-            initSoundPlaying = false;
-        });
+      initSoundPlaying = false;
+    });
 
-        super.initGame();
+    super.initGame();
 
-    }
+  }
 
-    /**
-     *
-     * Export data
-     * @method dataCollection
-     */
-    dataCollection() {
+  /**
+   *
+   * Export data
+   * @method dataCollection
+   */
+  dataCollection() {
 
-        let exportData = {
+    let exportData = {
 
-            ball_position_x: ball.position.x,
-            ball_position_y: ball.position.y,
-            paddle_position_x: paddle.position.x,
-            paddle_position_y: paddle.position.y,
+      ball_position_x: ball.position.x,
+      ball_position_y: ball.position.y,
+      paddle_position_x: paddle.position.x,
+      paddle_position_y: paddle.position.y,
 
-        };
+    };
 
-        super.storeData(exportData);
+    // this.context.get('export_arr').addObject(exportData);
+    super.storeData(exportData);
 
-    }
+  }
 
 }
