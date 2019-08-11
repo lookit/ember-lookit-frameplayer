f84086168b5019ca1dcb0a038dd11c4ac5b5a58d
glebiakovlev <g-iakovlev@northwestern.edu>
2019-08-11 19:57:09 +0900

    Remove export template

--- app/components/exp-lookit-games/component.js
+++ app/components/exp-lookit-games/component.js
@@ -71,24 +71,6 @@ export default ExpFrameBaseComponent.extend(FullScreen, VideoRecord, {
                     description: 'Game type  to display '
                 },
 
-                height: {
-                    type: 'number'
-                },
-                initV: {
-                    type: 'number'
-                },
-                flightTime: {
-                    type: 'number'
-                },
-                gravity: {
-                    type: 'number'
-                },
-                minTime: {
-                    type: 'number'
-                },
-                maxTime: {
-                    type: 'number'
-                },
                 /**
                  * Text to display for game instructions
                  *
@@ -245,13 +227,7 @@ export default ExpFrameBaseComponent.extend(FullScreen, VideoRecord, {
             this.startRecorder();
             this.hideRecorder();
             new Game(this, document, this.gameType);
-        },
-
-        export(){
-
-        this.jsonToCSVConvertor(this.export_arr,"Data",true);
-
-      }
+        }
     },
     // Other functions that are just called from within your frame can be defined here, on
     // the same level as actions and meta. You'll be able to call them as this.functionName(arguments)
@@ -271,80 +247,5 @@ export default ExpFrameBaseComponent.extend(FullScreen, VideoRecord, {
     // handlers. You can delete this if not doing anything additional.
     willDestroyElement() {
         this._super(...arguments);
-    },
-
-    jsonToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
-      //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
-      let arrData = typeof JSONData !== 'object' ? JSON.parse(JSONData) : JSONData;
-
-      let CSV = '';
-      //Set Report title in first row or line
-
-      CSV += ReportTitle + '\r\n\n';
-
-      //This condition will generate the Label/Header
-      if (ShowLabel) {
-        let row = "";
-
-        //This loop will extract the label from 1st index of on array
-        for (let index in arrData[0]) {
-
-          //Now convert each value to string and comma-seprated
-          row += index + ',';
-        }
-
-        row = row.slice(0, -1);
-
-        //append Label row with line break
-        CSV += row + '\r\n';
-      }
-
-      //1st loop is to extract each row
-      for (let i = 0; i < arrData.length; i++) {
-        let row = "";
-
-        //2nd loop will extract each column and convert it in string comma-seprated
-        for (let index in arrData[i]) {
-          row += '"' + arrData[i][index] + '",';
-        }
-
-        row.slice(0, row.length - 1);
-
-        //add a line break after each row
-        CSV += row + '\r\n';
-      }
-
-      if (CSV === '') {
-        alert("Invalid data");
-        return;
-      }
-
-      //Generate a file name
-      let fileName = "Report_";
-      //this will remove the blank-spaces from the title and replace it with an underscore
-      fileName += ReportTitle.replace(/ /g,"_");
-
-      //Initialize file format you want csv or xls
-      let uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
-
-      // Now the little tricky part.
-      // you can use either>> window.open(uri);
-      // but this will not work in some browsers
-      // or you will not get the correct file extension
-
-      //this trick will generate a temp <a /> tag
-      let link = document.createElement("a");
-      link.href = uri;
-
-      //set the visibility hidden so it will not effect on your web-layout
-      link.style = "visibility:hidden";
-      link.download = fileName + ".csv";
-
-      //this part will append the anchor tag and remove it after automatic click
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-  }
-
-
+    }
 });
