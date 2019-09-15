a2c291992f40f4e65d442c2f7ebb69df24d2756e
glebiakovlev <g-iakovlev@northwestern.edu>
2019-09-14 23:46:43 -0500

    Remove redundant instructions template

--- app/components/exp-lookit-game-instructions.js
+++
@@ -1,4 +0,0 @@
-// You should not need to edit this automatically-created file.
-
-import ExpLookitGameInstructions from '../../components/exp-lookit-game-instructions/component';
-export default ExpLookitGameInstructions;
