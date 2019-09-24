57a12b443df929bc1cd34561a34493e12205b12e
glebiakovlev <g-iakovlev@northwestern.edu>
2019-09-23 21:23:57 -0500

    Move static functions to Utils

--- app/components/exp-lookit-games/utils.js
+++ app/components/exp-lookit-games/utils.js
@@ -215,4 +215,31 @@ export default class Utils{
     return Utils.getArraysum(Utils.arrayProduct(timeVector, positionVector)) / Utils.getArraysum(Utils.arrayProduct(timeVector, timeVector));
   }
 
+
+  /**
+   * Fisher-Yates shuffle for uniform distribution
+   * @method shuffle
+   * @param {array} initial array
+   * @return {array} shuffled array
+   */
+  static shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+
 }
