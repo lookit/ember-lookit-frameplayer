393b3c04ce11934def2ea2c4c75b8b84f2236149
glebiakovlev <g-iakovlev@northwestern.edu>
2019-10-30 22:53:07 -0500

    Set current trajectory index as window

--- app/components/exp-lookit-games/discreteButtonSpatial.js
+++ app/components/exp-lookit-games/discreteButtonSpatial.js
@@ -512,7 +512,7 @@ export default class DiscreteButtonSpatial extends Base {
 
     let exportData = {
       game_type: 'discreteButtonSpatial',
-      window: currentTargetIndex+1,
+      window: this.getCorrectIndex(),
       selected_button: target_state + 1  ,
       obstruction_number: obstructions[super.currentRounds],
       ball_position_x: ball.position.x / this.canvas.width,
