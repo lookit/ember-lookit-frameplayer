708b01d04f0f039ade05c4b331ea598a977950d5
Kim Scott <kimscott@mit.edu>
2020-04-06 16:15:45 -0400

    style fixes

--- helpers.js
+++ helpers.js
@@ -1,16 +1,16 @@
 module.exports = {
     eq: function(item1, item2, block) {
         if (item1 == item2) {
-          return block.fn(this);
+            return block.fn(this);
         } else {
-          return block.inverse(this);
+            return block.inverse(this);
         }
     },
     noteq: function(item1, item2, block) {
         if (item1 == item2) {
-          return block.inverse(this);
+            return block.inverse(this);
         } else {
-          return block.fn(this);
+            return block.fn(this);
         }
     }
 };
