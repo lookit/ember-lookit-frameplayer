66e9b95b22257fd5da1388aac29f620ed1fc0677
CJ Green <44074998+okaycj@users.noreply.github.com>
2022-11-09 11:51:17 -0500

    Update s3 class to support aws-sdk v2

--- app/services/s3.js
+++ app/services/s3.js
@@ -1,85 +1,86 @@
-import { CreateMultipartUploadCommand, UploadPartCommand, CompleteMultipartUploadCommand, S3Client } from "@aws-sdk/client-s3";
-import Ember from 'ember';
+import ENV from '../config/environment';
 
 class S3 {
     constructor(key) {
-        this.env = Ember.getOwner(this).resolveRegistration('config:environment').s3;
+        this.env = ENV.s3;
         this.key = key;
         this.blobParts = [];
         this.promises = [];
         this.partNumber = 1;
-        this.client = new S3Client({
-            region: this.env.region,
+        // eslint-disable-next-line no-undef
+        this.s3 = new AWS.S3({
             credentials: {
-                accessKeyId: this.env.accesskeyId,
+                accessKeyId: this.env.accessKeyId,
                 secretAccessKey: this.env.secretAccessKey
             }
         });
     }
+
     get blobPartsSize() {
         return this.blobParts.reduce((p, c) => {
             return p + c.size;
         }, 0);
     }
+
     async createUpload() {
-        const createInput = {
-            Bucket: this.env.BUCKET,
+        const createResponse = await this.s3.createMultipartUpload({
+            Bucket: this.env.bucket,
             Key: this.key,
-            ContentType: "video/webm",
-        };
-        const create = new CreateMultipartUploadCommand(createInput);
-        const createResponse = await this.client.send(create);
+            ContentType: "video/mp4",
+        }).promise();
         this.uploadId = createResponse.UploadId;
     }
+
     async uploadPart(blob, partNumber) {
         let retry = 0;
         let err;
         while (retry < 3) {
             try {
-                const uploadInput = {
-                    Bucket: this.env.BUCKET,
-                    Key: this.key,
-                    UploadId: this.uploadId,
-                    PartNumber: partNumber,
+                const uploadPartResponse = await this.s3.uploadPart({
                     Body: blob,
-                };
-                const upload = new UploadPartCommand(uploadInput);
-                // upload file
-                const uploadPartResponse = await this.client.send(upload);
+                    Bucket: this.env.bucket,
+                    Key: this.key,
+                    PartNumber: partNumber,
+                    UploadId: this.uploadId,
+                }).promise();
+
                 return {
                     PartNumber: partNumber,
                     ETag: uploadPartResponse.ETag
                 };
-            }
-            catch (_err) {
+            } catch (_err) {
                 err = _err;
                 retry += 1;
             }
         }
         throw Error(`Upload part failed after 3 attempts. \n${err}`);
     }
+
     async completeUpload() {
         this.addUploadPartPromise();
         this.parts = await Promise.all(this.promises);
-        const completeInput = {
-            Bucket: this.env.BUCKET,
+
+        return this.s3.completeMultipartUpload({
+            Bucket: this.env.bucket,
             Key: this.key,
-            UploadId: this.uploadId,
-            MultipartUpload: { Parts: this.parts },
-        };
-        const complete = new CompleteMultipartUploadCommand(completeInput);
-        return await this.client.send(complete);
+            MultipartUpload: {
+                Parts: this.parts
+            },
+            UploadId: this.uploadId
+        }).promise();
     }
+
     addUploadPartPromise() {
         this.promises.push(this.uploadPart(new Blob(this.blobParts), this.partNumber++));
         this.blobParts = [];
     }
+
     onDataAvailable(blob) {
         this.blobParts.push(blob);
+
         if (this.blobPartsSize > 5 * (1024 * 1024)) {
             this.addUploadPartPromise();
         }
     }
 }
-
 export default S3;
