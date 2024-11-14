71240ed15f64208eb6a292a6ebb987f8eefc9c68
Becky Gilbert <beckyannegilbert@gmail.com>
2024-11-14 15:33:52 -0800

    remove custom RTD theme and replace with template override with docs tabs

--- app/conf.py
+++ app/conf.py
@@ -32,7 +32,7 @@ intersphinx_mapping = {'docs': ('https://lookit.readthedocs.io/en/develop/', Non
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = []
+templates_path = ['../_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -46,7 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_theme_path = ["../_themes/sphinx_rtd_theme"]
+#html_theme_path = ["../_themes/sphinx_rtd_theme"]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
